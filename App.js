75e373d5775b3aa1a1bb6286134bb4dc8504d046
chalalala <neko_hime1212@yahoo.com>
2020-09-29 20:42:23 +0700

    Add tabs

--- App.js
+++ App.js
@@ -1,13 +1,58 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import {NavigationContainer} from "@react-navigation/native";
+import {createStackNavigator} from "@react-navigation/stack";
+import { AntDesign } from '@expo/vector-icons'; 
+const Tab = createBottomTabNavigator();
+
+const Messages = () => {
+  return (
+    <View style={styles.container}>
+      {Messages.map(mess => (
+        <View style={styles.messageContainer}>
+          <View style={styles.authorText}>
+            {}
+          </View>
+        </View>
+      ))}
+    </View>
+  )
+}
+<Text>Messages</Text>;
+const Contacts = () => <Text>Contacts</Text>;
+const Groups() = () => <Text>Groups</Text>;
+const Timeline =() => <Text>Timeline</Text>;
+const More = () => <Text>More</Text>;
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused }) => {
+            <AntDesign
+              name={routeIcons[`${route.name}`]}
+              size={24}
+              color={focused ? "blue":"grey"}
+            >
+            </AntDesign>
+          ),
+        }}}
+        tabBarOptions={{
+          activeTintColor: "blue",
+          inactiveTintColor: "grey",
+        }}
+      >
+        <Tab.Screen options={{tabBarIcon: () => {
+          <AntDesign name="message1" size={24} color="black"/>
+        }}} name="Messages" component={Messages}/>
+        <Tab.Screen name="Contacts" component={Contacts}/>
+        <Tab.Screen name="Groups" component={Groups}/>
+        <Tab.Screen name="Timeline" component={Timeline}/>
+        <Tab.Screen name="More" component={More}/>
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -18,4 +63,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  authorText:{
+    fontSize:20,
+    
+  }
 });
